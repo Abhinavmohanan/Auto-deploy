838affe3c0952a669db49456dfbff28b6e522d99
Abhinav <abhinavmohanan17@gmail.com>
2024-02-20 23:03:17 +0530

    feat(reverse-proxy): Added dynamic reverse proxy for web apps

---
+++ apps/dynamic-reverse-proxy/src/index.ts
@@ -0,0 +1,30 @@
+import express from "express";
+import httpProxy from "http-proxy";
+
+require("dotenv").config();
+const app = express();
+
+const port = 5002;
+var proxy = httpProxy.createProxyServer();
+
+app.use((req, res) => {
+  const host = req.headers.host;
+  //Split subdomain from host
+  const subdomain = host?.split(".")[0];
+  console.log(subdomain);
+  return proxy.web(req, res, {
+    target: `https://adwebapps.blob.core.windows.net/${subdomain}`,
+    changeOrigin: true,
+  });
+});
+
+proxy.on("proxyReq", (proxyReq, req) => {
+  if (req.url === "/") {
+    proxyReq.path += "index.html";
+  }
+  return proxyReq;
+});
+
+app.listen(port, () =>
+  console.log(`Reverse proxy running on http://localhost:${port}`)
+);
