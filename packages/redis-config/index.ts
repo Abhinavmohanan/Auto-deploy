2b90386db04dfdeb8eeac3c254d370332bc98195
Abhinav <abhinavmohanan17@gmail.com>
2024-02-27 23:35:13 +0530

    feat (redis) : upgraded to redis image

--- packages/redis-config/index.ts
+++ packages/redis-config/index.ts
@@ -1,7 +1,7 @@
 import Redis from "ioredis";
 require("dotenv").config();
-const redisUri = process.env.REDIS_URI;
-export const redis = new Redis(redisUri!, {
+export const redis = new Redis("redis://redis:6379", {
+  port: 6379,
   autoResubscribe: true,
   retryStrategy: (times) => {
     const delay = Math.min(times * 50, 2000); // delay in milliseconds
