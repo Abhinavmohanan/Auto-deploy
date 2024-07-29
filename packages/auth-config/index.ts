e35f908e28d90d17261d387e315e6f572d47763e
Abhinav <abhinavmohanan17@gmail.com>
2024-07-30 03:01:24 +0530

    changes

--- packages/auth-config/index.ts
+++ packages/auth-config/index.ts
@@ -1,16 +1,16 @@
-export const appInfo = {
-  appName: "SuperTokens Autodeploy App",
-  apiDomain: "http://localhost:3002",
-  websiteDomain: "http://localhost:3000",
-  apiBasePath: "/auth",
-  websiteBasePath: "/auth",
-};
-
-// Prod
 // export const appInfo = {
-//   appName: "auto-deploy",
-//   apiDomain: "https://deployment.auto-deploy.tech",
-//   websiteDomain: "https://auto-deploy.tech",
+//   appName: "SuperTokens Autodeploy App",
+//   apiDomain: "http://localhost:3002",
+//   websiteDomain: "http://localhost:3000",
 //   apiBasePath: "/auth",
 //   websiteBasePath: "/auth",
 // };
+
+// Prod
+export const appInfo = {
+  appName: "auto-deploy",
+  apiDomain: "https://deployment.auto-deploy.tech",
+  websiteDomain: "https://auto-deploy.tech",
+  apiBasePath: "/auth",
+  websiteBasePath: "/auth",
+};
